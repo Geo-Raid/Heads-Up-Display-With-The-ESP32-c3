503611578156b7a94c44e8c2eca9ae4fb9ba9c3a
Geo-Raid <nathanosborne2009@outlook.com>
2026-08-13 20:41:03 +0100

    added the file for the esp32

---
+++ Heads_Up_Display_V1.ino
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <WiFi.h>
+
+// initialises all the vaiables and conststants
+const int DisplayWidth = 320;
+const int DisplayHeight = 172;
+
+// Wifi Details
+const char WiFiSSID[] = "GetOffMyWiFi";
+const char WiFiPassword[] = ""; // Add Password When Testing
+const
+
+void setup() {
+  // init code
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
